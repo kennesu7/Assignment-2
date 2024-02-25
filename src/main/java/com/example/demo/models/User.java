0da94e6ecd65fa53c4e9a61b25171d0ff361fe1d
Kenneth <kennethsj77@gmail.com>
2024-02-24 21:47:32 -0800

    second commit

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name="users")
 public class User {
+    //add id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int uid;
